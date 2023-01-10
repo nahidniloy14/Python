98b63ede9e9d3c106baf634945ed524bc4647e7b
nahidniloy14 <nahidniloysqa@gmail.com>
2023-01-10 22:51:55 +0600

    10/1/23

--- ProblemSolving/For Loop.py
+++ ProblemSolving/For Loop.py
@@ -108,3 +108,39 @@ for row  in range(4):
     for column in range(4-row):
         print('x',end="")
     print('\n')
+
+"""
+ * 
+
+      * * * 
+
+    * * * * * 
+
+  * * * * * * * 
+
+* * * * * * * * * 
+
+  * * * * * * *   
+
+    * * * * *     
+
+      * * *       
+
+        *         
+
+"""
+
+for row_1 in range(5):
+    for column in range(5-row_1-1):
+        print(' ',end=" ")
+    for column in range(row_1*2+1):
+        print('*',end=" ")
+    print('\n')
+for row_2 in range(5):
+    for column in range(row_2 + 1):
+        print(' ', end=" ")
+    for column in range(5- row_2*2+2):
+        print('*',end=" ")
+    for column in range(row_2 + 1):
+        print(' ',end=" ")
+    print('\n')
--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-for row  in range(4):
-    for column in range(4-row):
-        print('x',end="")
+for row_1 in range(5):
+    for column in range(5-row_1-1):
+        print(' ',end=" ")
+    for column in range(row_1*2+1):
+        print('*',end=" ")
+    print('\n')
+for row_2 in range(5):
+    for column in range(row_2 + 1):
+        print(' ', end=" ")
+    for column in range(5- row_2*2+2):
+        print('*',end=" ")
+    for column in range(row_2 + 1):
+        print(' ',end=" ")
     print('\n')
 
+#   0 1 2 3 4 5 6 7 8
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+8
