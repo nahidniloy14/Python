35fd368915c1d3d3fe52c8b499a7a99bcfbd2846
nahidniloy14 <nahidniloysqa@gmail.com>
2023-01-10 14:35:40 +0600

    10/1/23

---
+++ formattedStrings.py
@@ -0,0 +1,5 @@
+name="Aby"
+Age=27
+
+message=f'{name} {[Age]} is a patient in our hospital'
+print(message)
---
+++ strings.py
@@ -0,0 +1,24 @@
+#use of single quotes
+module='pyton'
+print(module)
+
+
+#use of double quotes
+course="python's course for beginners"
+print(course)#python's course for beginners
+print(course[:])
+print(course[-3]) #e
+print(course[0:6]) #python
+print(course[8:]) #course for beginners
+print(course[:6]) #python
+
+#use of 3 single quotes
+email= '''  
+Dear Sir,
+Thank you for the oppurtunity
+
+Regards,
+Nahid Hassan Niloy
+'''
+print(email)
+
