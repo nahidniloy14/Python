e6962715a498c406c9bfdc3d5549b392a0d1f64f
nahidniloy14 <nahidniloysqa@gmail.com>
2023-01-10 19:00:51 +0600

    10/1/23

--- ProblemSolving/While Loop.py
+++ ProblemSolving/While Loop.py
@@ -7,3 +7,43 @@ i=1
 while i<=100:
     print(i)
     i=i+1
+
+
+"""
+Number Guess
+"""
+secret_number=9
+guess_count=0
+guess_limit=3
+while guess_count<guess_limit:
+    guess=int(input("Guess: "))
+    guess_count +=1
+    if guess==secret_number:
+        print("You Win")
+        break
+else:
+    print("Sorry You Failed")
+
+"""
+Average Of 5 Numbers
+"""
+number_count=0
+number_limit=3
+total=0
+while number_count<number_limit:
+    number=int(input("Enter  a Number: "))
+    number_count+=1
+    total=total+number
+print("Average: ",+total/number_limit)
+
+"""
+Printing the square of numbers
+"""
+count=0
+limit=3
+while limit>count:
+    count=count+1
+    number=int(input("Enter a Number: "))
+    square=number*number
+    print("Square:",+square)
+
--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-temp=int(input("Temparature in Celcius Today: "))
-if temp>=30:
-    print("Weather is hot")
-elif temp<=20:
-    print("Weateher is cold")
-elif temp in range(20,30):
-    print("Weather is moderate")
-else:
-    print("Invalid")
 
