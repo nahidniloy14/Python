39beb000bb8b715801938cd01623ff104e9e19bf
nahidniloy14 <nahidniloy894@gmail.com>
2023-04-15 00:11:01 +0600

    14/4/23

--- Functions/ProblemSolving.py
+++ Functions/ProblemSolving.py
@@ -31,6 +31,13 @@ print(sum_series(n,a,r))
 """
 Check a number if it is even or not
 """
+#Method 1
+def even_check(number):
+    result=number%2==0
+    return result
+print(even_check(22))
+
+# Method 2
 def even_check(number):
     result=number%2==0
     return result
