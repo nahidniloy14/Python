41f439a8b2b4343c6812ac6f4c5efc0d11b2d0ee
nahidniloy14 <nahidniloy894@gmail.com>
2023-04-15 03:04:57 +0600

    Problem Description: Function to identify the employee with highest salary from a list

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+employee_details = [("Araf", "200"), ("Jabir", 400), ("Faruk", 350), ("Jony", 250)]
 
 
+def employee_check(employee_details):
+    highest_paid_employee = ''
+    max_salary = 0
+    for name,salary in employee_details:
 
-
-def check_even_list(number_list):
-    even_number=[]
-    for number in number_list:
-        if number%2==0:
-           even_number.append(number)
+        if salary > max_salary:
+            max_salary=salary
+            highest_paid_employee=name
         else:
             pass
-    return even_number
-print(check_even_list([12,11,33]))
+    return max_salary, highest_paid_employee
+
+
+print(employee_check(employee_details))
+
