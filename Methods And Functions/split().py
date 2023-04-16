60a9077711a77be2fe70c212f48da1259a071b1e
nahidniloy14 <nahidniloy894@gmail.com>
2023-04-16 23:51:34 +0600

    split function is added

---
+++ Methods And Functions/split().py
@@ -0,0 +1,18 @@
+"""
+Split function
+"""
+"""
+split function will split the string and return back to you as a  list
+"""
+"""
+syntax of split
+
+my_string.split(separator,max_split)
+"""
+my_string= "Hello World! I am learning python today"
+print(my_string.split())
+#['Hello', 'World!', 'I', 'am', 'learning', 'python', 'today']
+print(my_string.split("!",2))
+#['Hello World', ' I am learning python today']
+
+
